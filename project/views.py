d7f2b33d8ca13fdc9fde8f46a777bbb7dda6ed8b
Ario124 <47329632+Ario124@users.noreply.github.com>
2021-12-11 09:51:30 +0100

    Added templates with home and login inside project folder. Also added main.html and navbar.html.

--- project/views.py
+++ project/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'project/home.html')
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'project/login.html')
