3c0b0657cf4c4afbd2e777cd6f954357f82149c7
Ario124 <47329632+Ario124@users.noreply.github.com>
2021-12-11 09:38:58 +0100

    Added templates folder with home&login.html page. Added README and fixed requirements.txt since it was not displaying requirements.

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def home(request):
-    return HttpResponse('Hello World')
-
-def next_page(request):
-    return HttpResponse('Next page goes here')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('next_page/', next_page)
+    path('', include('project.urls')),
 ]
